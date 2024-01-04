555096cf276b85ff1321cde02aff1880b0945dcb
LIL_NAT\tuann <tuan.nguyen2208nat@hcmut.edu.vn>
2024-01-04 11:23:21 +0700

    HOC

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { withRouter } from "react-router-dom/cjs/react-router-dom.min";
-class Home extends React.Component {
+import { withRouter } from "react-router-dom";
+import Color from "../HOC/Color";
 
+class Home extends React.Component {
     componentDidMount() {
         setTimeout(() => {
-            this.props.history.push('todo')
+            this.props.history.push('todo');
         }, 3000);
     }
 
-
     render() {
-        console.log('>>>check props : ', this.props)
+        console.log('>>>check props : ', this.props);
         return (
-            <><div>
-                Hello world , This page of Lil Nat
-            </div></>
+            <>
+                <div>
+                    Hello world, This page of Lil Nat
+                </div>
+            </>
         );
     }
 }
-export default withRouter(Home);
+
+export default withRouter(Color(Home));
